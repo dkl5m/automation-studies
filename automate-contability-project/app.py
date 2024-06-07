09734034b1a879c7f486d9895cd4183526530577
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-06-06 22:44:18 -0300

    add automation box lines

--- automate-contability-project/app.py
+++ automate-contability-project/app.py
@@ -1,10 +1,37 @@
 import openpyxl
+import pyperclip
 #entrar na planilha
 
 workbook = openpyxl.load_workbook('produtos_ficticios.xlsx')
 sheet_products = workbook['Produtos']
 
 #copiar info de um campo e colar no seu campo correspondente
+for row in sheet_products.iter_rows(min_row=2):
+    # Product name
+    product_name = row[0].value
+    pyperclip.copy(product_name)
+    
+    # Product description
+    product_description = row[1].value
+    pyperclip.copy(product_description)
+    
+    # Product category
+    product_category = row[2].value
+    pyperclip.copy(product_category)
+    
+    # Product code
+    product_code = row[3].value
+    pyperclip.copy(product_code)
+    
+    # Product weight
+    product_weight = row[4].value
+    pyperclip.copy(product_weight)
+    
+    # Product dimensions
+    product_dimension = row[5].value
+    pyperclip.copy(product_dimension)
+    
+
 #repetir esses passos para outros campos até preencher campos da página
 #clicar em próximo
 #repetir os mesmos passos e ir para a próxima página (página 2)
