974745999bd2fc54bce3419403ab32047973f881
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-06-09 18:10:54 -0300

    Add third page contabil automate

--- automate-contability-project/app.py
+++ automate-contability-project/app.py
@@ -3,6 +3,8 @@ import pyperclip
 import pyautogui
 from time import sleep
 
+#site usado para cadastro: https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqazBjS0tXQWVDUVdoRUtYNk1XWmVGZmQyOGtTZ3xBQ3Jtc0ttUkJId01jNVlXOWgycG1pSmN3bjZIV1REdEY4NnQ5SXZjclFMdVg4aGtkenU1WWQzTGVkRUZlR2JSdmtpSDhNXzRDUGdlQXZOMlpwaVlWS015Nzl3aTJmRmVXVmthalNwbW1OSEtQUTlneTBJel9JYw&q=https%3A%2F%2Fcadastro-produtos-devaprender.netlify.app%2F&v=UtkPIpov6h8
+
 #entrar na planilha
 workbook = openpyxl.load_workbook('produtos_ficticios.xlsx')
 sheet_products = workbook['Produtos']
@@ -101,22 +103,45 @@ for row in sheet_products.iter_rows(min_row=2):
     # Product manufacturer
     product_manufacturer = row[12].value
     pyperclip.copy(product_manufacturer)
+    pyautogui.click(1470,345,duration=1)
+    pyautogui.hotkey('ctrl v')
     
     # Product country of origin
     product_country_of_origin = row[13].value
     pyperclip.copy(product_country_of_origin)
+    pyautogui.click(1470,425,duration=1)
+    pyautogui.hotkey('ctrl v')
     
     # Product observations
     product_observations = row[14].value
     pyperclip.copy(product_observations)
+    pyautogui.click(1470,525,duration=1)
+    pyautogui.hotkey('ctrl v')
     
     # Product bars_code
     product_bars_code = row[15].value
     pyperclip.copy(product_bars_code)
+    pyautogui.click(1470,655,duration=1)
+    pyautogui.hotkey('ctrl v')
     
     # Product warehouse_location
     product_warehouse_location = row[16].value
     pyperclip.copy(product_warehouse_location)
+    pyautogui.click(1470,735,duration=1)
+    pyautogui.hotkey('ctrl v')
+    
+    # Click finish button ("concluir")
+    pyautogui.click(1485,815,duration=1)
+    sleep(2)
+    # Click confirm inclusion button
+    pyautogui.click(1885,195,duration=1)
+    sleep(2)
+    # Click confirmation 2 button
+    pyautogui.click(1885,192,duration=1)
+    sleep(2)
+    # Click add one more button
+    pyautogui.click(1695,580,duration=1)
+    sleep(2)
     
 
 #repetir esses passos para outros campos até preencher campos da página
