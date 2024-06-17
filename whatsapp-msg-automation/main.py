ec448095346d3fe7a46a7489a5494315c78020d2
dkl5m <141453881+dkl5m@users.noreply.github.com>
2024-06-17 18:13:16 -0300

    Send msg to number

--- whatsapp-msg-automation/main.py
+++ whatsapp-msg-automation/main.py
@@ -7,6 +7,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
@@ -15,3 +16,19 @@ nav = webdriver.Chrome(service=service)
 nav.get("https://web.whatsapp.com")
 time.sleep(2*60)
 
+message = """Oi!
+Estou testando um recurso no whatsapp web.
+Entao, calma e nao bloqueia kk
+"""
+
+contact_list = ["Meu Numero", "Me, Myself and I", "Nós e eu", "EU, eu mesmo", "Grupo bom", "Gripe Mundial", "WTF is happening man"]
+
+# Enviar a mensagem para o Meu Numero para depois poder encaminhar
+# clicar na lupa
+nav.find_element('xpath', '//*[@id="side"]/div[1]/div/div/buttom/div[2]/span').click()
+# escrever Meu Numero
+nav.find_element('xpath', '//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p').send_keys("Meu Numero")
+# dar enter
+nav.find_element('xpath', '//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p').send_keys(Keys.ENTER)
+# escrever mensagem para Meu Numero
+# encaminhar mensagem para a lista de contatos
